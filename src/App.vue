c51c3fc1de89bd19f7afac5dcef6d92d7b88d75a
gg <qasemyq@yahoo.com>
2021-01-04 22:10:01 +0100

    cal

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col sm="6" offset-sm="3">
+              <Calculator/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style lang="scss">
+  
+</style>
+
